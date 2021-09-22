f17f735cb718dba2599d6613802775903711f5f3
Daniel Calvo <ImWizKat@gmail.com>
2021-09-22 12:49:44 +0200

    Agregar archivos de proyecto.

---
+++ EuromillonesDAM2022/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace EuromillonesDAM2022
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNumero1 = new System.Windows.Forms.TextBox();
+            this.textBoxNumero2 = new System.Windows.Forms.TextBox();
+            this.textBoxNumero3 = new System.Windows.Forms.TextBox();
+            this.textBoxNumero4 = new System.Windows.Forms.TextBox();
+            this.textBoxNumero5 = new System.Windows.Forms.TextBox();
+            this.textBoxNumero6 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxNumero1
+            // 
+            this.textBoxNumero1.Location = new System.Drawing.Point(54, 32);
+            this.textBoxNumero1.Name = "textBoxNumero1";
+            this.textBoxNumero1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumero1.TabIndex = 1;
+            this.textBoxNumero1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressParaValidar);
+            // 
+            // textBoxNumero2
+            // 
+            this.textBoxNumero2.Location = new System.Drawing.Point(54, 58);
+            this.textBoxNumero2.Name = "textBoxNumero2";
+            this.textBoxNumero2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumero2.TabIndex = 2;
+            this.textBoxNumero2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressParaValidar);
+            // 
+            // textBoxNumero3
+            // 
+            this.textBoxNumero3.Location = new System.Drawing.Point(54, 84);
+            this.textBoxNumero3.Name = "textBoxNumero3";
+            this.textBoxNumero3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumero3.TabIndex = 3;
+            this.textBoxNumero3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressParaValidar);
+            // 
+            // textBoxNumero4
+            // 
+            this.textBoxNumero4.Location = new System.Drawing.Point(54, 110);
+            this.textBoxNumero4.Name = "textBoxNumero4";
+            this.textBoxNumero4.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumero4.TabIndex = 4;
+            this.textBoxNumero4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressParaValidar);
+            // 
+            // textBoxNumero5
+            // 
+            this.textBoxNumero5.Location = new System.Drawing.Point(54, 136);
+            this.textBoxNumero5.Name = "textBoxNumero5";
+            this.textBoxNumero5.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumero5.TabIndex = 5;
+            this.textBoxNumero5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressParaValidar);
+            // 
+            // textBoxNumero6
+            // 
+            this.textBoxNumero6.Location = new System.Drawing.Point(54, 162);
+            this.textBoxNumero6.Name = "textBoxNumero6";
+            this.textBoxNumero6.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumero6.TabIndex = 6;
+            this.textBoxNumero6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.KeyPressParaValidar);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Yellow;
+            this.groupBox1.Controls.Add(this.textBoxNumero1);
+            this.groupBox1.Controls.Add(this.textBoxNumero6);
+            this.groupBox1.Controls.Add(this.textBoxNumero2);
+            this.groupBox1.Controls.Add(this.textBoxNumero5);
+            this.groupBox1.Controls.Add(this.textBoxNumero3);
+            this.groupBox1.Controls.Add(this.textBoxNumero4);
+            this.groupBox1.Location = new System.Drawing.Point(37, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 196);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mis 6 números";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Yellow;
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Location = new System.Drawing.Point(284, 50);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(204, 196);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Números ganadores";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(54, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(54, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(54, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(54, 136);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(54, 84);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(54, 110);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Yellow;
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Controls.Add(this.textBox9);
+            this.groupBox3.Controls.Add(this.textBox10);
+            this.groupBox3.Controls.Add(this.textBox11);
+            this.groupBox3.Controls.Add(this.textBox12);
+            this.groupBox3.Location = new System.Drawing.Point(533, 50);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(204, 196);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Números acertados";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(54, 32);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 1;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Enabled = false;
+            this.textBox8.Location = new System.Drawing.Point(54, 162);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 6;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Enabled = false;
+            this.textBox9.Location = new System.Drawing.Point(54, 58);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 2;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Enabled = false;
+            this.textBox10.Location = new System.Drawing.Point(54, 136);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 5;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Enabled = false;
+            this.textBox11.Location = new System.Drawing.Point(54, 84);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 3;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Enabled = false;
+            this.textBox12.Location = new System.Drawing.Point(54, 110);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(350, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(731, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Reiniciar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 279);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormPrincipal_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxNumero1;
+        private System.Windows.Forms.TextBox textBoxNumero2;
+        private System.Windows.Forms.TextBox textBoxNumero3;
+        private System.Windows.Forms.TextBox textBoxNumero4;
+        private System.Windows.Forms.TextBox textBoxNumero5;
+        private System.Windows.Forms.TextBox textBoxNumero6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EuromillonesDAM2022/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EuromillonesDAM2022
+{
+    public partial class FormPrincipal : Form
+    {
+        static ArrayList arrayReps = new ArrayList();
+        static int[] arrayUsuario = new int[6];
+        static int[] arrayResultado = new int[6];
+        static int[] arrayAciertos = new int[6];
+        static Random aleatorio = new Random();
+        
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void limpiar() {
+            foreach (Control ctrl in groupBox1.Controls)
+            {
+                ctrl.ResetText();
+            }
+        }
+
+        
+
+
+        private void FormPrincipal_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        /*
+        private void KeyPressParaValidar(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                TextBox textBox = (TextBox)sender;
+                if (textBox.Text.Equals(""))
+                {
+                    MessageBox.Show("Introduce un número");
+                }
+                else
+                {
+                    int num = Convert.ToInt16(textBox.Text);
+                    if (!validar(num))
+                    {
+                        MessageBox.Show("Introduce solo números del 0 al 50");
+                        textBox.Clear();
+                    }
+                    else
+                    {
+                        if (!validarRep(num))
+                        {
+                            MessageBox.Show("Ya has introducido ese número");
+                            textBox.Clear();
+                        }
+                        else
+                        {
+                            arrayReps.Add(num);
+                            textBox.Enabled = false;
+                        }
+                    }
+                }
+
+
+            
+            }
+        }
+        */
+
+
+        private bool validar(int num)
+        {
+            if (num <= 50 && num > 0)
+            {
+                return true;
+            }
+            else return false;
+
+        }
+
+        private bool validarRep(int num)
+        {
+            if (arrayReps.Contains(num))
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        private void KeyPressParaValidar(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                TextBox textBox = (TextBox)sender;
+                if (textBox.Text.Equals(""))
+                {
+                    MessageBox.Show("Introduce un número");
+                }
+                else
+                {
+                    int num = 0;
+                    try
+                    {
+                        num = Convert.ToInt16(textBox.Text);                   
+
+                    if (!validar(num))
+                    {
+                        MessageBox.Show("Introduce solo números del 0 al 50");
+                        textBox.Clear();
+                    }
+                    else
+                    {
+                        if (!validarRep(num))
+                        {
+                            MessageBox.Show("Ya has introducido ese número");
+                            textBox.Clear();
+                        }
+                        else
+                        {
+                            arrayReps.Add(num);
+                            textBox.Enabled = false;
+                        }
+                    }
+
+                    }
+
+                    catch (System.FormatException)
+                    {
+                        MessageBox.Show("Introduce solo números");
+                        textBox.Clear();
+                    }
+                }
+
+
+                int contador = 0;
+                foreach (Control ctrl in groupBox1.Controls)
+                {
+                    if (ctrl.Enabled == false) contador++;
+                }
+                if (contador == 6){
+                    button1.Enabled = true;                    
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            int i = 0;
+            foreach (Control ctrl in groupBox1.Controls)
+            {
+                arrayUsuario[i] = Convert.ToInt16(ctrl.Text);
+                i++;
+            }
+            arrayResultado =llenarArray();
+            i = 0;
+            foreach (Control ctrl in groupBox2.Controls)
+            {
+                ctrl.Text= arrayResultado[i].ToString();
+
+                i++;
+            }
+
+            for (int j = 0; j < 6; j++) {
+                if (arrayUsuario[j] == arrayResultado[j])
+                {
+                    arrayAciertos[j] = arrayUsuario[j];
+                }
+                else arrayAciertos[j] = -1;
+            }
+
+            i = 0;
+            foreach (Control ctrl in groupBox3.Controls)
+            {
+                ctrl.Text = arrayAciertos[i].ToString();
+
+                i++;
+            }
+
+        }
+
+        //MÉTODO PARA GENERAR LOS ALEATORIOS Y METERLOS EN UN ARRAY
+        private int[] llenarArray()
+        {
+            int[] array = new int[6];
+            int num;
+            int[] arrayTemp = new int[6];
+            for (int i = 0; i < array.Length; i++){
+                do{
+                    num = aleatorio.Next(0, 50);
+                }
+                while (array.Contains(num));
+                array[i] = num;
+            }
+            return array;
+
+        }
+
+        private void reset() {
+            arrayReps.Clear();
+            button1.Enabled = false;
+            foreach (Control ctrl in groupBox1.Controls) {
+                ctrl.ResetText();
+                ctrl.Enabled = true;
+            }
+            textBoxNumero1.Focus();
+            foreach (Control ctrl in groupBox2.Controls)
+            {
+                ctrl.ResetText();
+            }
+            foreach (Control ctrl in groupBox3.Controls)
+            {
+                ctrl.ResetText();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+    }
+}
